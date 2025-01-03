4732189eb89944e2261a7e388db5bc59dd6c189b
João Magalhães <joamag@gmail.com>
2025-01-03 22:42:56 +0000

    chore: new version value

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name="voucherify-api",
-    version="0.3.11",
+    version="0.1.0",
     author="Hive Solutions Lda.",
     author_email="development@hive.pt",
     description="Voucherify API Client",

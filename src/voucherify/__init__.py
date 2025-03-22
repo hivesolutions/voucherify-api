d6dbf0c4d568db80ec53d53b71dacf9675613108
João Magalhães <joamag@gmail.com>
2025-03-22 18:00:17 +0000

    fix: reference

--- src/voucherify/__init__.py
+++ src/voucherify/__init__.py
@@ -3,8 +3,10 @@
 
 from . import base
 from . import customer
+from . import order
 from . import validation
 
 from .base import BASE_URL, API
 from .customer import CustomerAPI
+from .order import OrderAPI
 from .validation import ValidationAPI
